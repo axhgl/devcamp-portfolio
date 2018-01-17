e0f702e26c97ad58f5ee44476364197373a1d259
Argjend Xhigoli <argjendxhigoli@gmail.com>
2018-01-17 15:47:35 +0100

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,4 +64,18 @@ module ApplicationHelper
     def active? path 
         "active" if current_page? path
     end
+
+    def alerts
+        alert = (flash[:alert] || flash[:error] || flash[:notice])
+        
+        if alert
+            alert_generator alert
+        end
+    end
+
+    def alert_generator msg
+            js add_gritter(msg, title: "Argjend Xhigoli Portfolio", sticky: false) 
+    end
 end
+
+
