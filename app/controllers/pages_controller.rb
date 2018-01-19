d046f1b5a7499d29aaabf98630f6328d2ac6c7a7
Argjend Xhigoli <argjendxhigoli@gmail.com>
2018-01-19 11:31:10 +0100

    Implemented twitter call on tech news page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,16 @@
 class PagesController < ApplicationController
   def home
-		@blogs = Blog.all
-		@skills = Skill.all
+    @blogs = Blog.all
+    @skills = Skill.all
   end
 
   def about
   end
 
   def contact
+  end
+
+  def tech_news
+    @tweets = SocialTool.twitter_search
   end
 end
